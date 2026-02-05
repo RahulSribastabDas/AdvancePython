1e223ddb17576057261cf9979ca669cd3cc8b08a
Rohit Kumar <pottamrohitks12@gmail.com>
2026-02-05 09:55:48 +0530

    author program.

---
+++ book_author_price_dict.py
@@ -0,0 +1,14 @@
+book_author_price={
+    "The Great Gatesby":{
+        "author":"Rohit Kumar",
+        "price":2999
+    },
+    "To kill a Mockingbird":{
+        "author":"Harper Lee",
+        "price":1999
+    },
+    "rich dad poor dad":{
+        "author":"Robert Kiyosaki",
+        "price":1499
+    }
+}
