023eecf615d7ff21ce480ef11b61e24fad1244a1
Rahul Sribastab Das <rahulsribastabdas.com>
2026-02-13 22:18:33 +0530

    add some python program

---
+++ grade.py
@@ -0,0 +1,13 @@
+n=int(input("Enter your mark:"))
+if(n>=90):
+    print("A+")
+elif(n>=80):
+    print("A")
+elif(n>=70):
+    print("B")
+elif(n>=60):
+    print("C")
+elif(n>=50):
+    print("D")
+else:
+    print("Fail")
